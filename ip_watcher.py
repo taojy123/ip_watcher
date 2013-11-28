6c661d937263edc343e3947a9d7aa9c48d2c36ab
Tao Jiayuan <taojy123@163.com>
2013-11-28 15:17:05 +0800

    1

--- ip_watcher.py
+++ ip_watcher.py
@@ -4,6 +4,7 @@ import urllib2
 from openpyxl import load_workbook
 
 filename = raw_input("请输入要处理的文件名，直接按下回车为默认文件名(IP地归.xlsx):")
+open("result.txt", "w").write("")
 
 if not filename:
     filename = 'IP地归.xlsx'
@@ -17,14 +18,16 @@ while True:
     n += 1
     if not sheet.cell('A' + str(n)).value:
         break
-    ip = sheet.cell('A' + str(n)).value
+    ip = str(sheet.cell('A' + str(n)).value)
     print ip,
     url = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?ip=%s" % ip
     r = urllib2.urlopen(url).read()
     rs = r.split()[3:]
-    address = " ".join(rs).decode("gbk")
+    address = " ".join(rs)
     print address
-    sheet.cell('B' + str(n)).value = address
+    sheet.cell('B' + str(n)).value = address.decode("gbk")
+    
+    open("result.txt", "a").write(ip + "," + address + "\n")
 
 wb.save('result.xlsx')
 raw_input("完成! 结果保存至 result.xlsx 文件")
